d183e5866facfa81130027a9e1c7e5df9929fbb9
boykely <valimo_ral@yahoo.fr>
2015-11-26 15:56:10 +0100

    +sketch UI

---
+++ IPSM/IPSM/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace IPSM
+{
+    partial class IPSM
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.gbParameters = new System.Windows.Forms.GroupBox();
+            this.pictureZone = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZone)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.gbParameters, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureZone, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(875, 409);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // gbParameters
+            // 
+            this.gbParameters.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbParameters.Location = new System.Drawing.Point(615, 3);
+            this.gbParameters.Name = "gbParameters";
+            this.gbParameters.Size = new System.Drawing.Size(257, 383);
+            this.gbParameters.TabIndex = 0;
+            this.gbParameters.TabStop = false;
+            this.gbParameters.Text = "User Parameters";
+            // 
+            // pictureZone
+            // 
+            this.pictureZone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureZone.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureZone.Location = new System.Drawing.Point(3, 3);
+            this.pictureZone.Name = "pictureZone";
+            this.pictureZone.Size = new System.Drawing.Size(606, 383);
+            this.pictureZone.TabIndex = 1;
+            this.pictureZone.TabStop = false;
+            // 
+            // IPSM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(875, 409);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "IPSM";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "IPSM";
+            this.Load += new System.EventHandler(this.IPSM_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.IPSM_Paint);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZone)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox gbParameters;
+        private System.Windows.Forms.PictureBox pictureZone;
+    }
+}
+
---
+++ IPSM/IPSM/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPSM
+{
+    public partial class IPSM : Form
+    {
+        /// <summary>
+        /// Bitmap is used to store the street graph when it is finished.
+        /// </summary>
+        Bitmap map;        
+        public IPSM()
+        {
+            InitializeComponent();            
+        }
+
+        private void IPSM_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IPSM_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = pictureZone.CreateGraphics();
+            g.PageUnit = GraphicsUnit.Pixel;
+            map = new Bitmap(pictureZone.Width, pictureZone.Height, g);
+            g.DrawString("Draw something here", new Font("Arial Black", 40), new SolidBrush(Color.Black), new PointF(5, 10));
+            g.DrawLine(new Pen(Color.Red), new PointF(0,250), new PointF(pictureZone.Width,250));
+            g.Dispose();
+        }        
+    }
+}
