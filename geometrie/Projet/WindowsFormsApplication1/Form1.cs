302388a5a616e8cecde490fc3f0b2e6fa58b93d4
boykely <valimo_ral@yahoo.fr>
2016-01-18 00:25:52 +0100

    + little update but still does not work

--- geometrie/Projet/WindowsFormsApplication1/Form1.cs
+++ geometrie/Projet/WindowsFormsApplication1/Form1.cs
@@ -17,7 +17,7 @@ namespace WindowsFormsApplication1
     {
         private Graphics g;
         private float[] centreP;
-        private string path = @"Datas\coeur_30.txt";
+        private string path = @"Datas\cercle_30.txt";
         private List<PointF> points;
         private List<PointF> points_octants;
         public Form1()
@@ -46,20 +46,23 @@ namespace WindowsFormsApplication1
                         foreach (Match ms in m)
                         {
                             float[] position = ChangeBase(float.Parse(ms.Groups["x"].Value), float.Parse(ms.Groups["y"].Value));
-                            if (float.Parse(ms.Groups["x"].Value) >= 0 && float.Parse(ms.Groups["y"].Value) >= 0)
+                            float x=float.Parse(ms.Groups["x"].Value);
+                            float y=float.Parse(ms.Groups["y"].Value);
+                            if ( x>= 0 &&  y>= 0 && y<x)
                             {
-                                map.SetPixel((int)position[0], (int)position[1], Color.Red);
+                                //map.SetPixel((int)position[0], (int)position[1], Color.Red);
+                                g.DrawRectangle(new Pen(Color.Red), (int)position[0], (int)position[1], 2, 2);
                                 points_octants.Add(new PointF(float.Parse(ms.Groups["x"].Value), float.Parse(ms.Groups["y"].Value)));
                             }
                             else
-                                map.SetPixel((int)position[0],(int) position[1], Color.Black);
+                                g.DrawRectangle(new Pen(Color.Black), (int)position[0], (int)position[1], 2, 2); //map.SetPixel((int)position[0], (int)position[1], Color.Black);
                                 
                             points.Add(new PointF(float.Parse(ms.Groups["x"].Value), float.Parse(ms.Groups["y"].Value)));
                         }
                         
                     }
-                    segmentNaiveLine(new PointF(0, 0), new PointF(10, 10), 3, 5,0, map);
-                    //premierOctant(points_octants.ToArray(),map);
+                    //segmentNaiveLine(new PointF(0, 0), new PointF(10, 10), 3, 5,0, map,g);
+                    premierOctant(points_octants.ToArray(),map,g);
                     g.DrawImage(map, new Point(0, 0));
                 }
             }
@@ -89,7 +92,7 @@ namespace WindowsFormsApplication1
         /// <summary>
         /// A segment of a naive line
         /// </summary>
-        private void segmentNaiveLine(PointF x0,PointF x1,int a,int b,int mu,Bitmap m)
+        private void segmentNaiveLine(PointF x0,PointF x1,int a,int b,int mu,Bitmap m,Graphics g)
         {
             PointF current = x0;
             int r = (int)(a * x0.X - b * x0.Y -mu);
@@ -112,60 +115,109 @@ namespace WindowsFormsApplication1
                 //points d'appui cf thèse partie 3.2
                 if (a * current.X - b * current.Y == mu)
                 {
-                    m.SetPixel(xx, yy, Color.Black);
+                    //m.SetPixel(xx, yy, Color.Black);
+                    g.DrawRectangle(new Pen(Color.Black), xx, yy, 2, 2);
                 }
                 else if (a * current.X - b * current.Y == (mu + b - 1))
                 {
-                    m.SetPixel(xx, yy, Color.Blue);
+                    //m.SetPixel(xx, yy, Color.Blue);
+                    g.DrawRectangle(new Pen(Color.Blue), xx, yy, 2, 2);
                 }
-                else m.SetPixel(xx, yy, Color.Red);
+                else g.DrawRectangle(new Pen(Color.Red), xx, yy, 2, 2);//m.SetPixel(xx, yy, Color.Red);
             }
         }
-        private void premierOctant(PointF[] E,Bitmap map)
+        private void premierOctant(PointF[] E,Bitmap map,Graphics g)
         {
-            //premier octant
-            int a = 0, b = 1, mu = 0,i=1;
-            PointF m = E[0];//1er points de E
-            PointF s0 = m, i0 = m;//points d'appui initial sup et inf
-            PointF p = new PointF();
-            PointF dPoint = new PointF();
-            bool segment = false;
-            int r;
-            while (i < E.Length)
+            int calc = 0;
+            PointF dm0 = new PointF(E[1].X - E[0].X, E[1].Y - E[0].Y);
+            PointF dmk;
+            PointF U, L;
+            int a, b;
+            int mu = 0;
+            //Initialisation
+            //find k greatest index
+            int k = 0;
+            for (int i = 0; i < E.Count(); i++)
             {
-                m = E[i];
-                //r =(int) (a * m.X - b * m.Y);
-                //segment = r >= mu - 1 && r <= mu+b;
-                //notion de distance vient de l'algo en anglais
-                dPoint = new PointF(E[i].X - E[i - 1].X, E[i].Y - E[i - 1].Y);
-                m.X += dPoint.X;
-                m.Y += dPoint.Y;
-                r = (int)(a * m.X - b * m.Y);
-                if (r == mu - 1 || r == mu + b)
+                dmk = new PointF(E[i].X - E[0].X, E[i].Y - E[0].Y);
+                if (dmk.X / i == dm0.X && dmk.Y / i == dm0.Y)
                 {
-                    if (r == mu - 1)
-                    {
-                        p.X = s0.X;
-                        p.Y = s0.Y;
-                        i0.X = m.X - b;
-                        i0.Y = m.Y - a - 1;
-                    }
-                    else if (r == mu + b)
-                    {
-                        p.X = i0.X;
-                        p.Y = i0.Y;
-                        s0.X = m.X - b;
-                        s0.Y = m.Y - a + 1;
-                    }
-                    b =(int)( m.X - p.X);
-                    a=(int)(m.Y-p.Y);
-                    mu =(int)( a * s0.X - b * s0.Y);
-                    //segmentNaiveLine(s0, p, a, b, mu, map);
-                    //float[] position = ChangeBase(p.X, p.Y);
-                    //map.SetPixel((int)position[0],(int) position[1], Color.Black);
+                    k = i;
                 }
-                i++;
             }
+            if (dm0.X == 1 && dm0.Y == 0)
+            {
+                a = 1;
+                b = k + 1;
+                U = new PointF(0, 0);
+                L = new PointF(k, 0);
+            }
+            else if (dm0.X == 1 && dm0.Y == 1)
+            {
+                a = 1;
+                b = 1;
+                U = new PointF(0, 0);
+                L = new PointF(0, 0);
+            }
+            else return;
+            //fin initialisation
+            while (k < E.Count())
+            {
+                dmk = new PointF(E[k + 1].X - E[k].X, E[k + 1].Y - E[k].Y);
+                if(dmk.X<=0 || dmk.Y<0)
+                {
+                    break;
+                }
+                PointF XX = new PointF(E[k].X + dmk.X, E[k].Y + dmk.Y);
+                int r =(int)( a * XX.X - b * XX.Y);
+                if (r < mu - 1 || r > mu + b) break;
+                k++;
+
+            }
+            
+            //premier octant
+            //int a = 0, b = 1, mu = 0,i=1;
+            //PointF m = E[0];//1er points de E
+            //PointF s0 = m, i0 = m;//points d'appui initial sup et inf
+            //PointF p = new PointF();
+            //PointF dPoint = new PointF();
+            //bool segment = false;
+            //int r;
+            //while (i < E.Length)
+            //{
+            //    m = E[i];
+            //    //r =(int) (a * m.X - b * m.Y);
+            //    //segment = r >= mu - 1 && r <= mu+b;
+            //    //notion de distance vient de l'algo en anglais
+            //    //dPoint = new PointF(E[i].X - E[i - 1].X, E[i].Y - E[i - 1].Y);
+            //    //m.X += dPoint.X;
+            //    //m.Y += dPoint.Y;
+            //    r = (int)(a * m.X - b * m.Y);
+            //    if (r == mu - 1 || r == mu + b)
+            //    {
+            //        if (r == mu - 1)
+            //        {
+            //            p.X = s0.X;
+            //            p.Y = s0.Y;
+            //            i0.X = m.X - b;
+            //            i0.Y = m.Y - a - 1;
+            //        }
+            //        else if (r == mu + b)
+            //        {
+            //            p.X = i0.X;
+            //            p.Y = i0.Y;
+            //            s0.X = m.X - b;
+            //            s0.Y = m.Y - a + 1;
+            //        }
+            //        b =(int)( m.X - p.X);
+            //        a=(int)(m.Y-p.Y);
+            //        mu =(int)( a * s0.X - b * s0.Y);
+            //        segmentNaiveLine(s0, p, a, b, mu, map,g);
+            //        float[] position = ChangeBase(p.X, p.Y);
+            //        //map.SetPixel((int)position[0],(int) position[1], Color.Black);
+            //    }
+            //    i++;
+            //}
         }
         void ExchangeAxis(int alpha, int beta, int gama, int delta, int phi, int psi)
         {
